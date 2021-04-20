99301feaacaf354c979d50042cd4d59fe3fefc15
ruivo-hash <61153290+ruivo-hash@users.noreply.github.com>
2021-04-20 00:06:11 -0300

    Add files via upload

---
+++ progamer/src/main/java/br/com/fiap/model/User.java
@@ -0,0 +1,54 @@
+package br.com.fiap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String birthDate;
+	private String email;
+	private String password;
+	
+	public User() {}
+
+	@Override
+	public String toString() {
+		return "User [ name=" + name + ", birthDate=" + birthDate + ", email=" + email + ", password="
+				+ password + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
